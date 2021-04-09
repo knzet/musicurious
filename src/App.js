6edf1651856b1485ad512a32db94ceebf75c55cd
ydp4388 <ydp4388@rit.edu>
2021-04-09 18:05:57 -0400

    Added the Top NavBar.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './styles/App.css';
 import './styles/Menu.css';
 import './styles/Profile.css';
 import './styles/Search.css';
+import './styles/TopBar.css'
 import 'react-tabs/style/react-tabs.css';
 import PageMenu from './components/PageMenu';
 import ProfileView from './components/ProfileView';
 import SearchView from './components/SearchView';
 import React, { Component } from 'react';
+import TopBar from "./components/TopBar";
 
 class App extends Component {
     constructor(props) {
@@ -59,46 +61,49 @@ class App extends Component {
                 {/* <div width="100%" background-color="red">
                 search
             </div> */}
-                <PageMenu
-                    className="Menu"
-                    navClick={this.navClick.bind(this)}
-                ></PageMenu>
-                {function (page) {
-                    if (page === 'profile') {
-                        return (
-                            <ProfileView
-                                user={{
-                                    userName: 'test Name',
-                                    group: false,
-                                    skills:
-                                        'Drummer, Singer, Guitarist, Pianoist, Formal music education, Songwriting',
-                                    goals: 'pro, looking for producer',
-                                }}
-                                className="Profile"
-                                handleFollowClick={this.handleFollowClick.bind(
-                                    this
-                                )}
-                                handleContactClick={this.handleContactClick.bind(
-                                    this
-                                )}
-                                group={true}
-                            ></ProfileView>
-                        );
-                    } else if (page === 'home') {
-                        <div>test div</div>;
-                    } else if (page === 'search') {
-                        return (
-                            <SearchView
-                                handleProfileClick={this.handleProfileClickFromSearch.bind(
-                                    this
-                                )}
-                            ></SearchView>
-                        );
-                    } else {
-                        return <div>attempting to render {page} page</div>;
-                    }
-                    // this line is wacky, the immediately invoked function needs to pass this as a parameter so it needs to be bound
-                }.bind(this)(this.state.page)}
+                <TopBar></TopBar>
+                <div className={"Content"}>
+                    <PageMenu
+                        className="Menu"
+                        navClick={this.navClick.bind(this)}
+                    ></PageMenu>
+                    {function (page) {
+                        if (page === 'profile') {
+                            return (
+                                <ProfileView
+                                    user={{
+                                        userName: 'test Name',
+                                        group: false,
+                                        skills:
+                                            'Drummer, Singer, Guitarist, Pianoist, Formal music education, Songwriting',
+                                        goals: 'pro, looking for producer',
+                                    }}
+                                    className="Profile"
+                                    handleFollowClick={this.handleFollowClick.bind(
+                                        this
+                                    )}
+                                    handleContactClick={this.handleContactClick.bind(
+                                        this
+                                    )}
+                                    group={true}
+                                ></ProfileView>
+                            );
+                        } else if (page === 'home') {
+                            <div>test div</div>;
+                        } else if (page === 'search') {
+                            return (
+                                <SearchView
+                                    handleProfileClick={this.handleProfileClickFromSearch.bind(
+                                        this
+                                    )}
+                                ></SearchView>
+                            );
+                        } else {
+                            return <div>attempting to render {page} page</div>;
+                        }
+                        // this line is wacky, the immediately invoked function needs to pass this as a parameter so it needs to be bound
+                    }.bind(this)(this.state.page)}
+                </div>
             </div>
         );
     }
