a579467ef41d4b03d991629dbdcf2e4289570768
vnp7514 <vnp7514@rit.edy>
2021-04-27 02:41:11 -0400

    Add a post text box

--- src/components/HomeView.js
+++ src/components/HomeView.js
@@ -5,6 +5,7 @@
         This is responsible for rendering the home page
  */
 import React from 'react';
+import Post from "./Post";
 
 export default class HomeView extends React.Component{
     constructor(props){
@@ -13,7 +14,50 @@ export default class HomeView extends React.Component{
             user: this.props.user,
             // a list of posts from friends, groups, members, collaborators
             feed: [],
+            // the text for the section where a new post is made
+            newText: '',
         };
+        this.handleSubmitInMakePost = this.handleSubmitInMakePost.bind(this);
+        this.handleChangeInMakePost = this.handleChangeInMakePost.bind(this);
+    }
+
+    /*
+    This updates the text for the new post section
+     */
+    handleChangeInMakePost(event){
+        this.setState({newText: event.target.value});
+    }
+
+    /*
+    This handles the creation of a new post and attached it to the current user
+     */
+    handleSubmitInMakePost(event){
+        this.state.feed.splice(0,0,
+            new Post({user: this.state.user, msg: this.state.newText}));
+        this.setState({});
+        event.preventDefault();
+    }
+
+    /*
+    This makes a form that looks like a post text box
+     */
+    renderMakePost(){
+        return (
+            <div className={'NewPost'}>
+              <form onSubmit={this.handleSubmitInMakePost}>
+                  <label>
+                      New Post:
+                      <br />
+                      <textarea value={this.state.newText}
+                                placeholder={'What you are thinking ...'}
+                                onChange={this.handleChangeInMakePost}
+                                required/>
+                      <br />
+                  </label>
+                  <input type="submit" value="Post"/>
+              </form>
+            </div>
+        );
     }
 
     /*
@@ -48,24 +92,41 @@ export default class HomeView extends React.Component{
         }
     }
 
+    /*
+    This renders a post
+    Params:
+        post    the post to render
+        index   the id of the post according to the state.feed
+     */
+    renderPost(post, index){
+        return(
+            <div className={'Post'} key={'Post'+index}>
+            <div className={'DisplayName'}
+                 onClick={() => {
+                     this.props.handleProfileClick(this.state.user)
+                 }}
+            >
+                {post.user.userName}
+            </div>
+            <div className={'PostContent'}>
+                {post.msg}
+            </div>
+            <div className={'Timestamp'}>
+                {post.time}
+            </div>
+        </div>
+        );
+    }
+
     render(){
         if (this.state.feed.length === 0) {
             this.getFeed();
         }
         return (
             <div className={'PostContainer'}>
+                {this.renderMakePost()}
                 {this.state.feed.map((post,index) => (
-                    <div className={'Post'} key={'Post'+index}>
-                        <div className={'DisplayName'}>
-                            {post.user.userName}
-                        </div>
-                        <div className={'PostContent'}>
-                            {post.msg}
-                        </div>
-                        <div className={'Timestamp'}>
-                            {post.time}
-                        </div>
-                    </div>
+                    this.renderPost(post, index)
                 ))}
             </div>
         );
