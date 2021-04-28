f8a236a7df8bc172b83c32b7f76b314b41f18825
vnp7514 <vnp7514@rit.edy>
2021-04-27 23:00:13 -0400

    Reformat how Account should work. Bio is no longer the trigger for default profile. Account will now check whether a property is undefined and assign default values

--- src/backend/Account.js
+++ src/backend/Account.js
@@ -1,6 +1,7 @@
 /*
     File: Account.js
     Author: Van Pham <vnp7514@rit.edu>
+            Kenn Taylor <kat6106@rit.edu>
     Description:
         This is the prototype (class) that represents an Account.
         If isUser is true, this is a User Account.
@@ -14,74 +15,43 @@ export default class Account {
 
     // constructor(name, isUser) {
     constructor(user) {
-        let bio = user.bio ? user.bio : null; // if we specify a bio in the construction, assume we are making a dummy user
-        let { userName, isUser } = user; // fun with js
-        if (bio) {
-            this.isUser = isUser; // true if this is a user, false if group
-            this.userName = userName; // Name of the account
-            this.contact = null; // Contact info
-            this.avatar = null; // Avatar picture
-            this.pictures = []; // Collection of pictures
-            this.skills = 'None'; // skills
-            this.bio = 'Description...'; // biography
-            this.songs = []; // songs
+        let {
+            isUser,
+            contact,
+            avatar,
+            userName,
+            goals,
+            pictures,
+            skills,
+            bio,
+            songs,
+            followers,
+            posts,
+            friends,
+            groups,
+            members,
+            collaborators,
+        } = user;
 
-            this.followers = []; // followers
-            this.goals = 'None'; // goals for search tool
-            this.location = 'Not shown'; // location
-            this.posts = []; // posts
+        this.userName = userName;
+        this.contact = contact;
+        this.avatar = avatar;
+        this.pictures = pictures;
+        this.skills = skills === undefined ? 'None' : skills;
+        this.goals = goals === undefined ? 'None' : goals;
+        this.bio = bio === undefined ? 'Description...' : bio;
+        this.songs = songs;
+        this.followers = followers === undefined ? [] : followers;
+        this.posts = posts === undefined ? [] : posts;
+        this.isUser = isUser;
 
-            // if this is a group then no friends
-            this.friends = isUser ? [] : null; // friends user has
-            // if this is a group then no groups
-            this.groups = isUser ? [] : null; // list of groups that user is in
-            // Only group can have members
-            this.members = isUser ? null : [];
-            // Only group can have collaborators
-            this.collaborators = isUser ? null : [];
-
-            this.addFriend = (user) => {
-                this.friends.push(user);
-            };
-        } else {
-            // dummy class so you can populate all fields by passing in a javascript object
-            let {
-                contact,
-                avatar,
-                userName,
-                goals,
-                pictures,
-                skills,
-                bio,
-                songs,
-                followers,
-                posts,
-                friends,
-                groups,
-                members,
-                collaborators,
-            } = user;
-
-            this.userName = userName;
-            this.contact = contact;
-            this.avatar = avatar;
-            this.pictures = pictures;
-            this.skills = skills;
-            this.goals = goals;
-            this.bio = bio;
-            this.songs = songs;
-            this.followers = followers;
-            this.posts = posts === undefined ? [] : posts;
-            this.isUser = isUser;
-
-            // if this is a group then no friends
-            this.friends = isUser ? (friends === undefined ? [] : friends) : null; // friends user has
-            // if this is a group then no groups
-            this.groups = isUser ? (groups === undefined ? [] : groups) : null; // list of groups that user is in
-            // Only group can have members
-            this.members = isUser ? null : (members === undefined ? [] : members);
-            // Only group can have collaborators
-            this.collaborators = isUser ? null : (collaborators === undefined ? [] : collaborators);
-        }
+        // if this is a group then no friends
+        this.friends = isUser ? (friends === undefined ? [] : friends) : null; // friends user has
+        // if this is a group then no groups
+        this.groups = isUser ? (groups === undefined ? [] : groups) : null; // list of groups that user is in
+        // Only group can have members
+        this.members = isUser ? null : (members === undefined ? [] : members);
+        // Only group can have collaborators
+        this.collaborators = isUser ? null : (collaborators === undefined ? [] : collaborators);
     }
 }
